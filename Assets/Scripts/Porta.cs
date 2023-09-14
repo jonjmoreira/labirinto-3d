a5fbcb5a0affd5f70f1ed4df8e69ccb24f957051
Jonathas Moreira <jonjmoreira@gmail.com>
2023-09-14 18:31:50 -0300

    versão 1.1. Adicionada perspectiva de velocidade, ainda necessário ajustar scripts das portas etc.

--- Assets/Scripts/Porta.cs
+++ Assets/Scripts/Porta.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Porta : MonoBehaviour
 {
-    public GameObject portaObj;
+    // public GameObject portaObj;
+    // private GameObject portaLadoE;
+    // private GameObject portaLadoD;
 
-    private void OnCollisionEnter(Collision other) {
-        portaObj.transform.rotation = new Quaternion(180,180,180, 0);
-        Debug.Log("Triggering");
-    }
+    // public void Setup()
+    // {
+    //     portaObj.SetActive(true);
+    //     portaLadoE = portaObj.transform.GetChild(0).gameObject;
+    //     portaLadoD = portaObj.transform.GetChild(1).gameObject;
+    // }
+
+    // private void OnCollisionEnter(Collision other) {
+    //     portaLadoE.transform.rotation = new Quaternion(0, 0, 0, 120);
+    //     Debug.Log("Triggering");
+    // }
 }
